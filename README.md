a3bf30b042e0bfb44c0048dc4edb7bb0d7725fd0
Milton Velilla <milton.velilla@overactive.com>
2021-09-06 17:27:00 -0500

    anchoring expressions

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ There are two ways to create reguar expresions
 {min, max} Matches min to max occurrences
 {min} Matches min occurrences
 {min,} Matches min or mor occurrences
+
+
+* Anchoring Metacharacters
+^ Anchors the match to he start of the line
+$ Anchors the match to the end of the line
+Note: multiline mode and global mode affect these metacharacters
+with both the regex will search at the beginning of each line (^) and at the end of each line ($) 
+\b word boundary - pattern bounded by a non-word character (\bplan\b will match exactly the words equals to plans not that contains plans)
+\B Nonword boundary - Pattern bounded by a word character (\Bplan\B will match the words that contain plan between letters)
--- meta-characters/meta-characters.js
+++ meta-characters/meta-characters.js
@@ -78,3 +78,13 @@ function secondExercise() {
     });
 })();
 
+
+function anchoringFunction() {
+    /*
+        The content.js file contains a string of text stored in a variable text1. This string of text is a statement that contains a day of the week as a part of the statement. Write a regular expression that will match any day of the week and then replace that day of the week in the string with Monday. Display the results to the console. 
+    */
+    let text1 = 'Each and every Tuesday, at the beginning of the day, we hold a staff meeting. At the Tuesday staff meeting, you will need to make a report on the past weeks progress, and you will receive assignments for the following Tuesday. Just be aware that somedays this Tuesday meeting might not occur. When that happens, we will make an announcement.';
+    const reg = /\b[mtwfs][a-z]{1,4}[nsir]day\b/gi;
+    const newText = text1.replace(reg, 'Monday');
+    console.log('anchoringFunction', newText);
+}
